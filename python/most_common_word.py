e5a6f06493b9790b31eccca1b5cbae1694c09612
Ronpoll <ronpollacking@gmail.com>
2025-12-23 10:06:55 +0200

    Create most_common_word.py

---
+++ python/most_common_word.py
@@ -0,0 +1,34 @@
+
+def read_book(book):
+    file1 = open(book, encoding='utf8').read()
+    clean_book = file1.replace('\n', " ").strip('.'+','+':'+'!'+'@'+'#'+'$'+'%'+'^'+'&'+'*'+'('+')'+'-'+'+'+'='+'—'+'?'+'_'+'”').split()
+    return  clean_book
+
+
+
+def most_common_word (book):
+    word_counter = {}
+    counter = 0
+    common_word = ""
+
+    for word in book:
+        word_counter[word] = 0
+
+    for index in range(len(book)):
+        if book[index] in word_counter:
+            word_counter[book[index]]+=1
+
+    for word in book:
+        if word_counter[word] > counter:
+            common_word = word
+            counter = word_counter[word]
+
+    return f'Most common word in text is: {common_word}\nNumber of times it is written: {counter}'
+
+def main(book_to_check):
+    book = read_book(book_to_check)
+    print(most_common_word(book))
+
+main('Alice in Wonderland.txt')
+
+
